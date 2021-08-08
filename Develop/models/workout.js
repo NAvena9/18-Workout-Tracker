d22b2a563802bc97006f7f47246c225b34ec0864
NOat <nicolas.cedano.avena@gmail.com>
2021-08-08 18:31:45 -0500

    Workkout route and Index route for module export created

---
+++ Develop/models/workout.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+//Schema definition
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            name: {
+                type: String,
+                trim: true,
+                required: "You must enter the name of the exercise"
+            },
+            type: {
+                type: String,
+                trim: true,
+                required: "You must enter the type of exercise"
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            },
+        }
+    ]
+});
+
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
